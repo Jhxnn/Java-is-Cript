1cb3042f46db63b90e02c2c3f7e19f07372b3004
Hilixx <jhonalizan@gmail.com>
2025-04-08 19:37:18 -0300

    first

---
+++ src/main/java/com/JavaisCript/JavaisCriptApplication.java
@@ -0,0 +1,13 @@
+package com.JavaisCript;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaisCriptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaisCriptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JavaisCript/JavaisCriptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JavaisCript;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaisCriptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
