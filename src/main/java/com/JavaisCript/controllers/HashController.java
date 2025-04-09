6c98506af4cbe2ef93209bb13798368231d90b21
Hilixx <jhonalizan@gmail.com>
2025-04-09 19:13:45 -0300

    controllers

--- src/main/java/com/JavaisCript/controllers/HashController.java
+++ src/main/java/com/JavaisCript/controllers/HashController.java
@@ -1,4 +1,45 @@
 package com.JavaisCript.controllers;
 
+import com.JavaisCript.dtos.HashDto;
+import com.JavaisCript.models.Hash;
+import com.JavaisCript.services.HashService;
+import jakarta.persistence.Access;
+import jakarta.persistence.GeneratedValue;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/hash")
 public class HashController {
+
+    @Autowired
+    HashService hashService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Hash> findById(@PathVariable(name = "id")UUID id){
+        return ResponseEntity.status(HttpStatus.OK).body(hashService.findById(id));
+    }
+    @GetMapping
+    public ResponseEntity<List<Hash>> findAll(){
+        return ResponseEntity.status(HttpStatus.OK).body(hashService.findAll());
+    }
+    @PostMapping
+    public ResponseEntity<Hash> createHash(@RequestBody HashDto hashDto){
+        return ResponseEntity.status(HttpStatus.CREATED).body(hashService.createHash(hashDto));
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Hash> updateHash(@PathVariable(name = "id")UUID id,
+                                           @RequestBody HashDto hashDto){
+        return ResponseEntity.status(HttpStatus.CREATED).body(hashService.updateHash(hashDto, id));
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Hash> deleteHash(@PathVariable(name = "id")UUID id){
+        hashService.deleteHash(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/JavaisCript/controllers/UserController.java
+++ src/main/java/com/JavaisCript/controllers/UserController.java
@@ -1,4 +1,47 @@
 package com.JavaisCript.controllers;
 
+import com.JavaisCript.dtos.UserDto;
+import com.JavaisCript.models.User;
+import com.JavaisCript.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable(name = "id") UUID id) {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.findById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody UserDto userDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.createUser(userDto));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable(name = "id") UUID id,
+                                           @RequestBody UserDto userDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.updateUser(userDto, id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<User> deleteUser(@PathVariable(name = "id") UUID id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
 }
