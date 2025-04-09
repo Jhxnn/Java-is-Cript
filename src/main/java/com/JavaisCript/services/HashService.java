ba9e0374e319cc943d4c5a3e81646dbedc7ed7c1
Hilixx <jhonalizan@gmail.com>
2025-04-09 19:05:02 -0300

    services

--- src/main/java/com/JavaisCript/services/HashService.java
+++ src/main/java/com/JavaisCript/services/HashService.java
@@ -1,4 +1,42 @@
 package com.JavaisCript.services;
 
+
+import com.JavaisCript.dtos.HashDto;
+import com.JavaisCript.models.Hash;
+import com.JavaisCript.repositories.HashRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
 public class HashService {
+
+    @Autowired
+    HashRepository hashRepository;
+
+    public Hash findById(UUID id){
+        return hashRepository.findById(id).orElseThrow(()-> new RuntimeException("Cannot be found"));
+    }
+    public List<Hash> findAll(){
+        return hashRepository.findAll();
+    }
+    public Hash createHash(HashDto hashDto){
+        var hash = new Hash();
+        BeanUtils.copyProperties(hashDto,hash);
+        return hashRepository.save(hash);
+    }
+    public Hash updateHash(HashDto hashDto, UUID id){
+        var hash = findById(id);
+        BeanUtils.copyProperties(hashDto,hash);
+        return hashRepository.save(hash);
+    }
+    public void deleteHash(UUID id){
+        var hash = findById(id);
+        hashRepository.delete(hash);
+
+    }
+
 }
--- src/main/java/com/JavaisCript/services/UserService.java
+++ src/main/java/com/JavaisCript/services/UserService.java
@@ -1,4 +1,43 @@
 package com.JavaisCript.services;
 
+import com.JavaisCript.dtos.UserDto;
+import com.JavaisCript.models.User;
+import com.JavaisCript.repositories.UserRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
 public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public User findById(UUID id){
+        return userRepository.findById(id).orElseThrow(() -> new RuntimeException("Cannot be found"));
+    }
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+
+    public User createUser(UserDto userDto){
+        var user = new User();
+        BeanUtils.copyProperties(userDto, user);
+        return userRepository.save(user);
+    }
+
+    public User updateUser(UserDto userDto, UUID id){
+        var user = findById(id);
+        BeanUtils.copyProperties(userDto, user);
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(UUID id){
+        var user = findById(id);
+        userRepository.delete(user);
+    }
 }
