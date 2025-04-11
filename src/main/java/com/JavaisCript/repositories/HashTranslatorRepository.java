4ee04556ad238de2c3094e8f3cd728d7455d629c
Hilixx <jhonalizan@gmail.com>
2025-04-11 15:05:01 -0300

    hashtranslator

---
+++ src/main/java/com/JavaisCript/repositories/HashTranslatorRepository.java
@@ -0,0 +1,9 @@
+package com.JavaisCript.repositories;
+
+import com.JavaisCript.models.HashTranslator;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface HashTranslatorRepository extends JpaRepository<HashTranslator, UUID> {
+}
