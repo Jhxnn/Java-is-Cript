1db0ce8db17c376558bf2823ef142a3af8d1a311
Hilixx <jhonalizan@gmail.com>
2025-04-15 18:19:49 -0300

    hash delete

--- src/main/java/com/JavaisCript/dtos/HashTranslatorDto.java
+++ src/main/java/com/JavaisCript/dtos/HashTranslatorDto.java
@@ -3,5 +3,5 @@ package com.JavaisCript.dtos;
 public record HashTranslatorDto (String a,String b,String c,String d,String e,String f,String g,String h,
                                  String i,String j,String k,String l,String m,String n,String o,
                                  String p,String q,String r,String s,String t,String u,String v,String w,
-                                 String x,String y,String z){
+                                 String x,String y,String z, String key){
 }
